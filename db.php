f74b29e7686f24a3e35d3afacf97452f7350d132
m-logeshkumar <23cseb39.logeshkumar.m@gmail.com>
2025-07-27 01:02:22 +0530

    Modified DB.php

--- db.php
+++ db.php
@@ -3,7 +3,8 @@ $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
-$conn =new mysqli($host, $user, $pass, $db);
+
+$conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
