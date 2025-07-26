8b87952c6e0bbd3c03d5a10f4c957572c7853e70
m-logeshkumar <23cseb39.logeshkumar.m@gmail.com>
2025-07-27 00:45:01 +0530

    Added Env for DB

--- db.php
+++ db.php
@@ -1,5 +1,9 @@
 <?php
-$conn = new mysqli("localhost", "root", "root", "medi_stack");
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
+$conn =new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
