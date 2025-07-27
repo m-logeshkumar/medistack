743fafb98f0ad8c3414fa048aad09199a93407bd
m-logeshkumar <23cseb39.logeshkumar.m@gmail.com>
2025-07-27 11:50:23 +0530

    Modified db.php

--- db.php
+++ db.php
@@ -1,10 +1,5 @@
 <?php
-$host = getenv("DB_HOST") ?: "db";
-$user = getenv("DB_USER") ?: "root";
-$pass = getenv("DB_PASS") ?: "root";
-$db   = getenv("DB_NAME") ?: "medi_stack";
-
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli("localhost", "root", "root", "medi_stack");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
